3d646d3ebe641f7c08b2da9348cc7c4278bb0d7a
panhasim <panha.simwestern001@gmail.com>
2024-09-23 21:03:18 -0700

    - Finish up Footer, Footer is Responsive.

--- src/app/components/CommitedToSuccess.tsx
+++ src/app/components/CommitedToSuccess.tsx
@@ -5,16 +5,16 @@ export default function CommitedToSuccess(){
     return(
         <>
         <div className="gradient">
-            <div className="container-xxl">
-                <div className="commited-to-success-container">
+            <div className="container">
+                <div className="row commited-to-success-container">
 
-                    <div className="commited-to-success-text">
+                    <div className="col-sm-12 col-lg-6 commited-to-success-text">
                         <h1>COMMITED TO YOUR SUCCESS</h1>
                         <p>I am dedicated to providing exceptional professionalism and expertise in guiding you through your real estate journey. My commitment is to support you in achieving your broader life goals with precision and care. Whether navigating the complexities of buying, selling, or relocating, my focus is on delivering a sophisticated and seamless experience tailored to your unique needs and aspirations.</p>
                         <Button name="VIEW LISTING" type="primary"/>
                     </div>
 
-                    <div className="mayra-picture"></div>
+                    <div className="col-sm-12 col-lg-6 mayra-picture"></div>
                 </div>
             </div>
         </div>
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,54 @@
 import "../styles/footer.css"
+import Image from 'next/image';
+import WestUSA from "../assets/WestUSA.svg"
+import MLS from "../assets/MLS.svg"
+import EqualHousingOpportunity from "../assets/Equal Housing Opportunity.svg"
 
 export default function Footer(){
     return(
         <> 
+        <div className="container">
+            <div className="footer-container">
+                <Image
+                className="row mb-5"
+                priority
+                src={WestUSA}
+                alt="Follow us on Twitter"
+                />
 
+                <div className="row mb-5 footer-info">
+                    <div className="col-sm-6 col-xl-3 word-block first-word-block">
+                        <h3 className="text-heading-dark mb-2">MAYRA MESSER</h3>
+                        <p className="text-body-dark">(623) 707-7957</p>
+                        <p className="text-body-dark">maymesser@gmail.com</p>
+                    </div>
+                    <div className="col-sm-6 col-xl-3 word-block ">
+                        <h3 className="text-heading-dark mb-2">MAYRA MESSER</h3>
+                        <p className="text-body-dark">2920 N. Litchfield Road, Suite #100 Goodyear, AZ 85395</p>
+                    </div>
+                </div>
+
+                <p className="text-body-dark">WestUSA Realty® and the WestUSA Realty Logo are service marks licensed to WestUSA Realty LLC and used with permission. WestUSA Realty fully supports the principles of the Fair Housing Act and the Equal Opportunity Act. Each office is independently owned and operated. Any services or products provided by independently owned and operated offices are not provided by, affiliated with, or related to WestUSA Realty LLC nor any of its affiliated companies.</p>
+                <div className="row mb-5">
+                    <Image
+                    className="col-1 SVGsize"
+                    priority
+                    src={EqualHousingOpportunity}
+                    alt="Follow us on Twitter"
+                    />
+                    <Image
+                    className="col-1 SVGsize"
+                    priority
+                    src={MLS}
+                    alt="Follow us on Twitter"
+                    />
+                </div>
+
+                <hr></hr>
+
+            </div>
+
+        </div>
         </>
     )
 }
