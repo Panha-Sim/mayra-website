8c461c97a64eb9c25f969665be11cca89bc39ee1
panhasim <panha.simwestern001@gmail.com>
2024-09-28 09:44:01 -0700

    Install Bootstrap 5.3.3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 // import localFont from "next/font/local";
 import "./globals.css";
-import "./styles/bootstrap-grid.rtl.min.css"
+// import "./styles/bootstrap-grid.rtl.min.css"
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 export default function RootLayout({
   children,
