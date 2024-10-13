0f5c12bc2c4822f04fd76c4cd9d0b4d268971fe4
panhasim <panha.simwestern001@gmail.com>
2024-10-13 09:57:39 -0700

    - Small media query change for the about page, modified the "let's work together" statement.

--- src/components/LetsWorkTogether.tsx
+++ src/components/LetsWorkTogether.tsx
@@ -9,7 +9,7 @@ export default function LetsWorkTogether() {
                 <div className="row">
                     <div className="work-together-text">
                         <h1>LET’S GET TO WORK TOGETHER</h1>
-                        <p>Ready to turn your real estate dreams into reality? Partner with me, and together, I'll make every step of the journey seamless and successful. Let's make it happen!</p>
+                        <p>Ready to turn your real estate dreams into reality? With years of experience volunteering in my children's school PTA and helping families with income taxes, I've learned the true value of communication and dedication. Partner with me, and I'll bring the same care and responsibility to make every step of your journey seamless and successful. Let's make it happen!</p>
                         <Button name="GET IN TOUCH" slug="contact"/>
                     </div>
                 </div>
