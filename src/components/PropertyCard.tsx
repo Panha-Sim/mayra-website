e5fe42143b861e87b88ab6f62c6eba7d3379e049
panhasim <panha.simwestern001@gmail.com>
2024-10-20 21:10:14 -0700

    - Populate listing data and fix picture aspect ratio

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -6,18 +6,20 @@ export default function PropertiesCard(props: any) {
     return (
         <div className="property-card">
             <div className="property-image-container">
-                <img src={`${props.image}`} className="house-image" alt="house"/>
+                <img src={` ${props.image} `} className="house-image" alt="house"/>
             </div>
 
             <div className="property-info">
                 <h2><strong> { props.price } </strong></h2>
-                <h4 className="mt-3"><strong> { props.community } </strong></h4>
-                <p className="address"> { props.address } </p>
+                <div>
+                    <h4><strong> { props.community } </strong></h4>
+                    <p className="address"> { props.address } </p>
+                </div>
 
                 <div className="estate-badge-container">
-                    <EstateBade type="bed" num ={`${props.beds} beds`}/>
-                    <EstateBade type="bath" num ={`${props.baths} Baths`}/>
-                    <EstateBade type="sqft" num ={`${props.sqft} sqft`}/>
+                    <EstateBade type="bed" num ={` ${props.beds} beds `}/>
+                    <EstateBade type="bath" num ={` ${props.baths} Baths `}/>
+                    <EstateBade type="sqft" num ={` ${props.sqft} sqft `}/>
                 </div>
             </div>
 
